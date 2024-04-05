1bde3995b0aaef84c9067b86c1662aaab31968ce
vinitpatil2812 <vinitpatil280@gmail.com>
2024-04-05 14:55:24 +0530

    EditorPage setup

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
 
 const EditorPage = () => {
+    const [clients, setClients] = useState([
+        {socketId:1, username: "Hello"},
+        {socketId:2, username: "Hii"},
+        {socketId:3, username: "Hii"},
+        {socketId:4, username: "Hii"},
+        {socketId:5, username: "Hii"},
+        {socketId:6, username: "Hii"},
+        {socketId:7, username: "Hii"},
+    ]);
+
+
     return (
-        <div>Editor Page</div>
+        <div className="mainWrapper">
+            <div className="leftSide">
+                <div className="leftSideInner">
+                    <h2>Connected Users</h2>
+                    <div className="clientsList">
+                        {
+                            clients.map((client) => (
+                                <Client key={client.socketId} username={client.username} />
+                            ))
+                        }
+                    </div>
+                </div>
+                <button>Copy RoomId</button>
+                <button className="leaveBtn">Leave</button>
+            </div>
+            <div className="editor">
+                <Editor />
+            </div>
+        </div>
     )
 }
 
